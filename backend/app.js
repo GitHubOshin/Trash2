d32c716c6106de1e6a70727f7d4fcd4b9fc41db3
OSHIN <izkp44@gmail.com>
2023-03-08 23:27:41 +0700

    Created port

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,12 @@
 import express from 'express'
 import Quote from 'inspirational-quotes'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(Quote.getQuote())
+})
+
+app.listen(9200, () => {
+  console.log('Server started successfully! 🥳')
+})
