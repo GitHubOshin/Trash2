53d7669489644c69d690dd981c93ab527aa659ad
OSHIN <izkp44@gmail.com>
2023-03-09 01:21:23 +0700

    Solved the error

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,15 @@ import Quote from 'inspirational-quotes'
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send(Quote.getQuote())
 })
